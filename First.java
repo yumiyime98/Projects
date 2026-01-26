9b20efeb4f4fc59b1d4a8f46f88a5eab69661f2f
yumiyime (Lynn F.) <115372846+yumiyime98@users.noreply.github.com>
2026-01-26 13:45:14 -0500

    First Java

---
+++ First.java
@@ -0,0 +1,14 @@
+//Lynne Roden
+//CPSC 1302K
+//Module 1
+
+public class First
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundo!");
+        System.out.println("Job Title: Game Developer");
+        System.out.println("Why?: I've always had a hobby for video games ever since I was a kid and wanted to create one of my own.");
+        System.out.println("Median Salary: $100,000/yr");
+    }
+}
